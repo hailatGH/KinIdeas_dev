e423e83855641497cc989eb612c936cbf2c557d8
Hailemichael Atrsaw <hailat.alx@gmail.com>
2022-07-06 16:38:38 +0300

    hot fix

--- media_service/src/core/settings.py
+++ media_service/src/core/settings.py
@@ -84,12 +84,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -176,3 +176,14 @@ CORS_ALLOW_HEADERS = [
     "x-csrftoken",
     "x-requested-with",
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kinmusic_media',
+        'USER': 'admin',
+        'PASSWORD': 'playground',
+        'HOST': '10.191.0.3',
+        'PORT': '',
+    }
+}
